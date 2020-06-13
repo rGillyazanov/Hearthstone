f6e8e9f4f9c1c7fb7abfc4b6b0b59909e0791f6b
rGillyazanov <rgillyazanov@yandex.ru>
2020-06-13 12:59:23 +0300

    Добавлена валидация по полю сортировки карт

--- Hearthstone/app/Http/Requests/Hearthstone/Cards/SearchCards.php
+++ Hearthstone/app/Http/Requests/Hearthstone/Cards/SearchCards.php
@@ -32,7 +32,25 @@ class SearchCards extends FormRequest
             'packset' => 'exists:packsets,id',
             'cost' => 'numeric|min:0',
             'attack' => 'numeric|min:0',
-            'health' => 'numeric|min:0'
+            'health' => 'numeric|min:0',
+            'sort' => ['json', function ($attribute, $value, $fail) {
+                $this->sorted($attribute, $value, $fail);
+            }]
         ];
+    }
+
+    /**
+     * Проверяет, чтобы поля и типы сортировки, соответствовали
+     * типам, указанным в форме.
+     * @param $attribute
+     * @param $value
+     * @param $fail
+     */
+    private function sorted($attribute, $value, $fail) {
+        $value = json_decode($value);
+        if (!($value->name == 'cost' || $value->name == 'attack' || $value->name == 'health')
+            || !($value->type == 'asc' || $value->type == 'desc')) {
+            $fail($value->name.' or '.$value->type.' is invalid.');
+        }
     }
 }
